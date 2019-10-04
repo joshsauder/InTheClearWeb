3adb0ddd9e584d74b509f25fe73314066e2acbf3
Josh Sauder <joshsauder@gmail.com>
2019-10-03 21:21:22 -0400

    initial work on polyline done

--- frontend/src/components/PolylineGenerator.js
+++ frontend/src/components/PolylineGenerator.js
@@ -12,10 +12,12 @@ class PolylineGenerator extends Component {
 
     generatePolyline(start, end, map){
         var path;
+        var steps
         return axios.get(`http://localhost:3400/api/directions/${start.lat},${start.lng}/${end.lat},${end.lng}`)
         .then(response => {
             path = window.google.maps.geometry.encoding.decodePath(response.data.points);
-
+            steps = response.data.steps
+            weatherPerStep(steps, path)
             delete response.data.points
             return axios.post("http://localhost:3400/api/directions/info", response.data)
         })
@@ -25,6 +27,7 @@ class PolylineGenerator extends Component {
 
             for (var i = 0; i < path.length; i++) {
                 bounds.extend(path[i]);
+
               }
             
               var polyline = new window.google.maps.Polyline({
@@ -46,6 +49,30 @@ class PolylineGenerator extends Component {
         return axios.post("http://localhost:3400/api/directions/info", steps)
     }
 
+
+    weatherPerStep(steps, path){
+        let i = 0;
+        let numSegs = []
+        steps.forEach(step => {
+            let ret = determineSegCount(step, path, i)
+            i = ret[0]
+            numSegs.push(ret[1])
+        })
+    }
+
+    determineSegCount(step, path, index){
+
+        let i = index
+        let numberSegs = 1
+
+        while(abs(path[i].lat - step.end_location.lat) > 0.3 || abs(path[i].lng - step.end_location.lng )> 0.3 ){
+
+            numberSegs += 1
+            i += 1
+        }
+
+        return ([i, numberSegs])
+    }
 
 }
 
