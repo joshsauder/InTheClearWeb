d994e021950c07f62f45657d805ce05734c34764
Josh Sauder <joshsauder@gmail.com>
2019-10-12 17:42:12 -0400

    frontend deploys

--- server/index.js
+++ server/index.js
@@ -1,38 +1,21 @@
 var express = require('express')
 var app = express()
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
 const routes = require('./routes')
 var cors = require('cors')
 const bodyParser = require('body-parser')
 
 var port = process.env.PORT || 3400;
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
 
 app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+}
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-}));
-
 app.use('/api', routes)
 app.listen(port, function()
-{console.log(`Server is listening on port 3400`)})
+{console.log(`Server is listening on port ${port}`)})
 module.exports = app;
