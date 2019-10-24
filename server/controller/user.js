86db753978117582efe2586623a5df80de85c13f
Josh Sauder <joshsauder@gmail.com>
2019-10-23 22:34:10 -0400

    added signin controller function

--- server/controller/user.js
+++ server/controller/user.js
@@ -13,3 +13,19 @@ exports.createUser = function(req, res){
     })
 
 }
+
+exports.signInUser = function(req, res){
+
+    let user = new user(req.body);
+
+    user.findOne({ userName: user.username}), function(err, user){
+        if(err) res.status(500).send("User does not exist")
+
+        user.comparePassword(user.password, function(err, isMatch){
+            if(err){ res.status(500).send("Incorrect Password") }
+            else{
+                res.send("success")
+            }
+        })
+    }
+}
