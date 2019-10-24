e1cc601a42303a64d9d3709a55ffbe7cd24d5755
Josh Sauder <joshsauder@gmail.com>
2019-10-23 22:15:59 -0400

    removed logging

--- server/controller/user.js
+++ server/controller/user.js
@@ -4,7 +4,6 @@ const user = require('../model/user')
 exports.createUser = function(req, res){
 
     let newUser = new user(req.body);
-    console.log(newUser)
 
     newUser.save((err, user) => {
         if(err){
