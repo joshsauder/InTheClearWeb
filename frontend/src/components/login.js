00940bbd711617f1c464692dbe861bf4858e8fa1
Josh Sauder <joshsauder@gmail.com>
2019-10-29 22:25:05 -0400

    added submit user

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -7,12 +7,15 @@ class Login extends Component {
     constructor(props){
         super(props)
         this.state = {
-            username: '',
-            password: ''
+            username: "",
+            password: "",
+            email: "",
+            login: true
         }
 
         this.handleInputChange = this.handleInputChange.bind(this)
         this.onSubmit = this.onSubmit.bind(this)
+        this.submitNewUser = this.submitNewUser.bind(this)
     }
 
     handleInputChange = (event) => {
@@ -43,11 +46,35 @@ class Login extends Component {
         
     }
 
+    submitNewUser = (event) => {
+        event.preventDefault();
+
+        const userObj = {
+            username: this.state.username, 
+            password: this.state.password,
+            email: this.state.email
+        }
+
+        Axios.post('api/user', userObj)
+        .then(res => {
+            if(res.status == 200){
+                this.setState({login: true})
+            }
+        }).catch(err => {
+            alert("There was an issue signing you up! Please try again.")
+        })
+    }
+
+    handleNewUser = () => {
+        this.setState({login: false})
+    }
+
     render(){
         return(
             <Card>
                 <Card.Header>Login</Card.Header>
                 <Card.Body>
+                {this.state.login ?
                     <form onSubmit={this.onSubmit}>
                         <div className="form-group">
                             <label htmlFor="username">Username</label>
@@ -58,7 +85,26 @@ class Login extends Component {
                             <input name="password" className="form-control" defaultValue={this.state.password} onChange={this.handleInputChange}></input>
                         </div>
                         <Button type="submit">Submit</Button>
+                        <Button onClick={() => {this.handleNewUser}}>Register</Button>
                     </form>
+                    :
+                    <form onSubmit={this.submitNewUser}>
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input name="username" className="form-control" defaultValue={this.state.username} onChange={this.handleInputChange}></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input name="password" className="form-control" defaultValue={this.state.password} onChange={this.handleInputChange}></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email Adress</label>
+                            <input name="email" className="form-control" defaultValue={this.state.password} onChange={this.handleInputChange}></input>
+                            <small className="form-text text-muted">We will never share nor spam your Email Address</small>
+                        </div>
+                        <Button type="submit">Submit</Button>
+                    </form>
+                }
                 </Card.Body>
             </Card>
         )
