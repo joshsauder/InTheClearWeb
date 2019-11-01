9565249d7a296694d52ecf77dd15f7208af43890
Josh Sauder <joshsauder@gmail.com>
2019-10-31 22:41:36 -0400

    image full screen

--- frontend/src/components/loginContainer.js
+++ frontend/src/components/loginContainer.js
@@ -1,14 +1,12 @@
 import React, {component} from 'react';
 import '../style/login.css'
 import logo from '../images/InTheClear.png'
-import background from '../images/background.jpg'
 
 const LoginContainer = (props) => {
 
     return(
-        <div>
-            <img src={background} style={{position: "fixed"}}></img>
-            <img className="mx-auto d-block img-logo mb-1" style={{position: "relative"}} alt="logo" src={logo}></img>
+        <div className="imageBackground">
+            <img className="mx-auto d-block img-logo mb-1 mt-3" style={{position: "relative"}} alt="logo" src={logo}></img>
             {props.children}
         </div>
     )
