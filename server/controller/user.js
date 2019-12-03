e560af79eb6bc20c8759c642ad864f5226b1e1bd
Josh Sauder <joshsauder@gmail.com>
2019-12-02 23:05:24 -0500

    fixed user bug

--- server/controller/user.js
+++ server/controller/user.js
@@ -25,24 +25,25 @@ exports.signInUser = function(req, res){
     //find user
     User.findOne({ username: userAuth.username}, function(err, user){
         if(err) res.status(500).send("Server error")
-        if(!user){res.status(401).send("User not found")}
+        else if(!user){res.status(401).send("User not found")}
+        else{
+            //compare password
+            user.comparePassword(userAuth.password, function(err, isMatch){
+                if(!isMatch){ res.status(401).send("Incorrect Password") }
+                else{
 
-        //compare password
-        user.comparePassword(userAuth.password, function(err, isMatch){
-            if(!isMatch){ res.status(401).send("Incorrect Password") }
-            else{
-
-                const payload = {username: userAuth.username}
-                //create token
-                const token = jwt.sign(payload, secret, {
-                    expiresIn: '1h'
-                  });
-                res.cookie('token', token, { httpOnly: true })
-                //send frontend api key on callback since the key will be exposed in .env on frontend.
-                //https://create-react-app.dev/docs/adding-custom-environment-variables/
-                res.json({api_KEY: process.env.GOOGLE_MAPS_FRONTEND_KEY})
-            }
-        })
+                    const payload = {username: userAuth.username}
+                    //create token
+                    const token = jwt.sign(payload, secret, {
+                        expiresIn: '1h'
+                    });
+                    res.cookie('token', token, { httpOnly: true })
+                    //send frontend api key on callback since the key will be exposed in .env on frontend.
+                    //https://create-react-app.dev/docs/adding-custom-environment-variables/
+                    res.json({api_KEY: process.env.GOOGLE_MAPS_FRONTEND_KEY})
+                }
+            })
+        }
     })
 }
 
