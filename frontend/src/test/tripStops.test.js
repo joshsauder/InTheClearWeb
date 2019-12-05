b128ad1b1f1b3c9ed538b550cc2b1ae5b9a56a24
Josh Sauder <joshsauder@gmail.com>
2019-12-04 22:25:00 -0500

    updated test cases

--- frontend/src/test/tripStops.test.js
+++ frontend/src/test/tripStops.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow, mount } from 'enzyme';
-import TripStops from '../components/TripStops';
+import TripStopsModal from '../components/TripStops/TripStopsModal';
+import RouteDataView from '../components/TripStops/RouteDataView'
 import 'jest-enzyme'
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
@@ -18,29 +19,35 @@ describe('Test Trip Stops', () => {
         name: "Apple HQ"
     }
 
-    test("Test Render", () => {
-        const tripStopsComponent = shallow(<TripStops show={false} start={startCoordinates} end={endCoordinates} />)
+    test("Test Modal Render", () => {
+        const tripStopsComponent = shallow(<TripStopsModal show={true} />)
 
         expect(tripStopsComponent.exists()).toBe(true);
 
     })
 
-    test("Test Intial State", () => {
-        const tripStopsComponent = shallow(<TripStops show={true} start={startCoordinates} end={endCoordinates} />)
+    test("Test Route View Render", () => {
+        const date = [new Date()]
+        const tripStopsComponent = shallow(<RouteDataView start={startCoordinates} end={endCoordinates} date={date} minDate={[]} />)
+        expect(tripStopsComponent.exists()).toBe(true);
+    })
 
-        //assert one item... init state doesnt render destination
-        expect(tripStopsComponent.find('.boxedItem').length).toEqual(1)
+    test("Test Intial State", () => {
+        const date = [new Date()]
+        const tripStopsComponent = shallow(<RouteDataView show={true} start={startCoordinates} end={endCoordinates} date={date} minDate={date}/>)
+
+        //both start and end should render
+        expect(tripStopsComponent.find('.boxedItem').length).toEqual(2)
     })
 
     test("Test Submit Button", () => {
 
         const mockFn = jest.fn()
-        const tripStopsComponent = shallow(<TripStops show={true} start={startCoordinates} end={endCoordinates} callback={mockFn}/>)
+        const tripStopsComponent = shallow(<TripStopsModal show={true} submit={mockFn}/>)
 
         tripStopsComponent.find("Button").simulate("Click")
 
         expect(mockFn).toHaveBeenCalled();
-
 
     })
 })
